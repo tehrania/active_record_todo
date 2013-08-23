dcfab6bb646c881117d34db8e1c162bae3b91580
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-23 15:17:34 -0700

    create tasks table migration

---
+++ db/201308221556_create_tasks_table.rb
@@ -0,0 +1,11 @@
+require_relative '../config'
+
+class CreateTasks <ActiveRecord::Migration
+  def change
+
+    create_table :tasks do |t|
+      t.string :name
+      t.string :status
+    end
+end
+
